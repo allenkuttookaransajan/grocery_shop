128bb82294baef14d1277f303443e83fb9bebad2
Allen <allenksajan@gmail.com>
2023-10-12 23:02:53 +0530

    cart tile size

--- lib/pages/cart_page.dart
+++ lib/pages/cart_page.dart
@@ -12,109 +12,114 @@ class CartPage extends StatelessWidget {
       appBar: AppBar(
         backgroundColor: Colors.transparent,
         elevation: 0,
-        iconTheme: IconThemeData(color: Colors.black),
+        iconTheme: const IconThemeData(color: Colors.black),
       ),
-      body: Consumer<CartModel>(builder: (context, value, child) {
-        return Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            Padding(
-              padding: const EdgeInsets.symmetric(horizontal: 24.0),
-              child: Text(
-                "My Cart",
-                style: GoogleFonts.notoSerif(
-                  fontSize: 40,
-                  fontWeight: FontWeight.bold,
+      body: Consumer<CartModel>(
+        builder: (context, value, child) {
+          return Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Padding(
+                padding: const EdgeInsets.symmetric(horizontal: 24.0),
+                child: Text(
+                  "My Cart",
+                  style: GoogleFonts.notoSerif(
+                    fontSize: 40,
+                    fontWeight: FontWeight.bold,
+                  ),
                 ),
               ),
-            ),
-            Expanded(
-              child: ListView.builder(
-                itemCount: value.cartItems.length,
-                padding: EdgeInsets.all(12.0),
-                itemBuilder: (context, index) {
-                  return Padding(
-                    padding: const EdgeInsets.all(12.0),
-                    child: Container(
-                      decoration: BoxDecoration(
+              Expanded(
+                child: ListView.builder(
+                  itemCount: value.cartItems.length,
+                  padding: const EdgeInsets.all(12.0),
+                  itemBuilder: (context, index) {
+                    return Padding(
+                      padding: const EdgeInsets.all(12.0),
+                      child: Container(
+                        padding: EdgeInsets.all(15),
+                        decoration: BoxDecoration(
                           color: Colors.grey[200],
-                          borderRadius: BorderRadius.circular(8)),
-                      child: ListTile(
-                        leading: Image.asset(
-                          value.cartItems[index][2],
-                          height: 36,
+                          borderRadius: BorderRadius.circular(8),
                         ),
-                        title: Text(value.cartItems[index][0]),
-                        subtitle: Text('\$' + value.cartItems[index][1]),
-                        trailing: IconButton(
-                          icon: Icon(Icons.cancel),
-                          onPressed: () =>
-                              Provider.of<CartModel>(context, listen: false)
-                                  .removeItemFromCart(index),
-                        ),
-                      ),
-                    ),
-                  );
-                },
-              ),
-            ),
-            Padding(
-              padding: const EdgeInsets.all(36.0),
-              child: Container(
-                decoration: BoxDecoration(
-                  color: Colors.green,
-                  borderRadius: BorderRadius.circular(8),
-                ),
-                padding: EdgeInsets.all(24),
-                child: Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                  children: [
-                    Column(
-                      crossAxisAlignment: CrossAxisAlignment.start,
-                      children: [
-                        Text(
-                          "Total Price",
-                          style: TextStyle(color: Colors.green[100]),
-                        ),
-                        Text(
-                          '\$${value.calculateTotal()}',
-                          style: TextStyle(
-                            color: Colors.white,
-                            fontSize: 18,
-                            fontWeight: FontWeight.bold,
+                        child: ListTile(
+                          leading: Image.asset(
+                            value.cartItems[index][2],
+                            height: 50,
                           ),
-                        )
-                      ],
-                    ),
-                    Container(
-                      decoration: BoxDecoration(
-                        border: Border.all(
-                          color: Colors.green.shade100,
+                          title: Text(value.cartItems[index][0]),
+                          // ignore: prefer_interpolation_to_compose_strings
+                          subtitle: Text('\$' + value.cartItems[index][1]),
+                          trailing: IconButton(
+                            icon: const Icon(Icons.cancel),
+                            onPressed: () =>
+                                Provider.of<CartModel>(context, listen: false)
+                                    .removeItemFromCart(index),
+                          ),
                         ),
-                        borderRadius: BorderRadius.circular(12),
                       ),
-                      padding: EdgeInsets.all(12),
-                      child: Row(
+                    );
+                  },
+                ),
+              ),
+              Padding(
+                padding: const EdgeInsets.all(36.0),
+                child: Container(
+                  decoration: BoxDecoration(
+                    color: Colors.green,
+                    borderRadius: BorderRadius.circular(8),
+                  ),
+                  padding: const EdgeInsets.all(24),
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
                         children: [
                           Text(
-                            "Pay Now",
-                            style: TextStyle(color: Colors.white),
+                            "Total Price",
+                            style: TextStyle(color: Colors.green[100]),
                           ),
-                          Icon(
-                            Icons.arrow_forward_ios,
-                            size: 16,
-                            color: Colors.white,
+                          Text(
+                            '\$${value.calculateTotal()}',
+                            style: const TextStyle(
+                              color: Colors.white,
+                              fontSize: 18,
+                              fontWeight: FontWeight.bold,
+                            ),
                           )
                         ],
                       ),
-                    )
-                  ],
+                      Container(
+                        decoration: BoxDecoration(
+                          border: Border.all(
+                            color: Colors.green.shade100,
+                          ),
+                          borderRadius: BorderRadius.circular(12),
+                        ),
+                        padding: const EdgeInsets.all(12),
+                        child: const Row(
+                          children: [
+                            Text(
+                              "Pay Now",
+                              style: TextStyle(color: Colors.white),
+                            ),
+                            Icon(
+                              Icons.arrow_forward_ios,
+                              size: 16,
+                              color: Colors.white,
+                            )
+                          ],
+                        ),
+                      )
+                    ],
+                  ),
                 ),
-              ),
-            )
-          ],
-        );
-      }),
+              )
+            ],
+          );
+        },
+      ),
     );
   }
 }
--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -60,25 +60,26 @@ class HomePage extends StatelessWidget {
               child: Consumer<CartModel>(
                 builder: (context, value, child) {
                   return GridView.builder(
-                      itemCount: value.shopItems.length,
-                      padding: const EdgeInsets.all(12),
-                      gridDelegate:
-                          const SliverGridDelegateWithFixedCrossAxisCount(
-                        crossAxisCount: 4,
-                        childAspectRatio: 1 / 1.4,
-                      ),
-                      itemBuilder: (context, index) {
-                        return GroceryItemTile(
-                          itemName: value.shopItems[index][0],
-                          itemPrice: value.shopItems[index][1],
-                          imagePath: value.shopItems[index][2],
-                          color: value.shopItems[index][3],
-                          onPressed: () {
-                            Provider.of<CartModel>(context, listen: false)
-                                .addItemToCart(index);
-                          },
-                        );
-                      });
+                    itemCount: value.shopItems.length,
+                    padding: const EdgeInsets.all(12),
+                    gridDelegate:
+                        const SliverGridDelegateWithFixedCrossAxisCount(
+                      crossAxisCount: 4,
+                      childAspectRatio: 1 / 1.4,
+                    ),
+                    itemBuilder: (context, index) {
+                      return GroceryItemTile(
+                        itemName: value.shopItems[index][0],
+                        itemPrice: value.shopItems[index][1],
+                        imagePath: value.shopItems[index][2],
+                        color: value.shopItems[index][3],
+                        onPressed: () {
+                          Provider.of<CartModel>(context, listen: false)
+                              .addItemToCart(index);
+                        },
+                      );
+                    },
+                  );
                 },
               ),
             )
